4bb95a4ad9e439a0470a2fd7fdc5f1ffe7760992
erncak <erincak2@gmail.com>
2025-04-03 18:52:32 +0300

    order controller-order related dtos created

---
+++ services/order/src/main/java/com/erincak/ecommerce/order/OrderService.java
@@ -0,0 +1,11 @@
+package com.erincak.ecommerce.order;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+}
---
+++ services/order/src/main/java/com/erincak/ecommerce/order/PaymentMethod.java
@@ -0,0 +1,9 @@
+package com.erincak.ecommerce.order;
+
+public enum PaymentMethod {
+ PAYPAL,
+ CREDIT_CARD,
+ VISA,
+ MASTERCARD,
+ BITCOIN
+}
